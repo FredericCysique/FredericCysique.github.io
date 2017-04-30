24ca6042681c1bb8bc69d1cbf55af19b5db4caee
FredericCysique <Frederic.cysique@hotmail.fr>
2017-05-01 00:35:38 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
+$(document).ready(function() {
+	$.ajax({
+		url : "https://api.blockcypher.com/v1/btc/main",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
 
+		success : function(data) {
+			$('#bitcoin_block_number').append(data.height);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_block_number').append(err+" N/A");
+		}
+	});
+});
+$(document).ready(function() {
+	$.ajax({
+		url : "https://api.blockcypher.com/v1/btc/main",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_network_hash').append(data.time);
+		},
+
+		error : function(xhr, status, err) {
+			$('#bitcoin_network_hash').append(err+" N/A");
+		}
+	});
+});
